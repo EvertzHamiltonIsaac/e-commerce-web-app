e8f4d658b3f0ceb6c899673e19ac2c35badfd799
Yelysz <anyelystorres17@gmail.com>
2023-06-28 16:51:59 -0400

    Arreglos header

--- src/pages/OurStore.jsx
+++ src/pages/OurStore.jsx
@@ -74,7 +74,7 @@ const OurStore = () => {
                   <div className="form-floating">
                     <input
                       type="email"
-                      className="form-control"
+                      className="form-control"    
                       id="floatingInput"
                       placeholder="From"
                     />
