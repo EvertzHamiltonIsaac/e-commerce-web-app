d7f8a601914a184f71c9670bb0b9b2ffa8a7542b
saulmarte <saulkjh1227@gmail.com>
2023-07-20 11:27:56 -0400

    Changed get to post enquiry

--- src/features/contact/contactService.js
+++ src/features/contact/contactService.js
@@ -3,7 +3,7 @@ import { base_url, config } from "../../utils/axiosConfig";
 
 const postQuery = async (contactData) => {
   try {
-    const response = await axios.get(`${base_url}enquiry`, contactData);
+    const response = await axios.post(`${base_url}enquiry`, contactData);
     return response.data;
   } catch (error) {
     console.error("Error retrieving contacts:", error);
